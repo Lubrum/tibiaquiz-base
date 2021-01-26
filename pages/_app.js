112271d8db09741b4f87da5101f050e40b93033c
Lubrum <lucianobrum18@gmail.com>
2021-01-26 20:43:37 -0300

    Aula 2 - imersao alura - state, linter e router.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import db from '../db.json'
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+import db from '../db.json';
 
-const GlobalStyle = createGlobalStyle `
+const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
   }
@@ -11,7 +13,8 @@ const GlobalStyle = createGlobalStyle `
     /* New styles */
     display: flex;
     flex-direction: column;
-    font-family: 'Lato', sans-serif;
+    font-family: 'Potta One', cursive;
+    //font-family: 'Lato', sans-serif;
     // Deixa branco no começo
     color: ${({ theme }) => theme.colors.contrastText};
   }
@@ -23,17 +26,23 @@ const GlobalStyle = createGlobalStyle `
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme
+const { theme } = db;
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
-    return ( 
-      <>
-        <ThemeProvider theme = { theme }>
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Potta+One&display=swap" rel="stylesheet" />
+      </Head>
+      <ThemeProvider theme={theme}>
         <GlobalStyle />
-          <Component {...pageProps } />  
-        </ThemeProvider>
-      </>
-    )
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import Footer from '../src/components/Footer'
-import GithubCorner from '../src/components/GithubCorner'
-import QuizBackground from '../src/components/QuizBackground'
+import React from 'react';
+import Head from 'next/head';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GithubCorner from '../src/components/GithubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -17,27 +22,49 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Alura Quiz - Modelo Base</title>
+      </Head>
       <QuizContainer>
-
+        <QuizLogo />
         <Widget>
-          <Widget.Header> 
+          <Widget.Header>
             <h1>Tibia</h1>
-          </Widget.Header> 
+          </Widget.Header>
           <Widget.Content>
-            <p>Tibia Quiz Upcoming ;)</p>
+            <form onSubmit={function (e) {
+              e.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              // router manda para a próxima página //
+            }}
+            >
+              <input
+                placeholder="Informe seu apelido"
+                onChange={function (info) {
+                  setName(info.target.value);
+                }}
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
         <Widget>
-            <Widget.Content>
-              <p>215421 5152 1212 51! </p>
-            </Widget.Content>
+          <Widget.Content>
+            <p>215421 5152 1212 51! </p>
+          </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
-      <GithubCorner projectUrl="http://github.com/Lubrum"/>
+      <GithubCorner projectUrl="http://github.com/Lubrum" />
     </QuizBackground>
-  )
+  );
 }
