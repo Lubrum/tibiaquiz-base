9e1ec330dc2bfdd6b8cdf2f451c50e77f761d276
Lubrum <lucianobrum18@gmail.com>
2021-01-30 01:16:05 -0300

    Aula 5 Alura concluida!

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
 import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,9 +1,40 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import QuizScreen from '../../src/screens/Quiz';
 
-export default function QuizDaGaleraPage() {
+export default function QuizDaGaleraPage({ dbExterno }) {
   return (
-    <div>
-      Desafio da próxima aula junto com as animações
-    </div>
+    <ThemeProvider theme={dbExterno.theme}>
+      <QuizScreen
+        externalQuestions={dbExterno.questions}
+        externalBg={dbExterno.bg}
+      />
+    </ThemeProvider>
   );
+}
+
+// eslint-disable-next-line no-unused-vars
+export async function getServerSideProps(context) {
+  try {
+    const [projectName, githubUser] = context.query.id.split('___');
+    const dbExterno = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Falha em pegar os dados');
+      })
+      .then((responseJson) => responseJson)
+      .catch((err) => {
+        console.log(err);
+      });
+    return {
+      props: {
+        dbExterno,
+      },
+    };
+  } catch (err) {
+    throw new Error(err);
+  }
 }
