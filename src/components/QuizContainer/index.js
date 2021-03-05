dd2508b2f17e7de0983f8887a637d1f4c69559b9
Luciano Brum <luciano.brum@wplex.com.br>
2021-03-04 21:32:34 -0300

    More responsive design

--- src/components/QuizContainer/index.js
+++ src/components/QuizContainer/index.js
@@ -1,13 +1,44 @@
 import styled from 'styled-components';
 
 const QuizContainer = styled.div`
-  width: 30%;
-  position: relative; 
-  left: 50%;
-  transform: translateX(-50%);
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
+  /* Extra small devices (phones, 600px and down) */
+  @media only screen and (max-width: 600px) {
+    width: 90%;
+    position: relative; 
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  /* Small devices (portrait tablets and large phones, 600px and up) */
+  @media only screen and (min-width: 600px) {
+    width: 80%;
+    position: relative; 
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  /* Medium devices (landscape tablets, 768px and up) */
+  @media only screen and (min-width: 768px) {
+    width: 70%;
+    position: relative; 
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  /* Large devices (laptops/desktops, 992px and up) */
+  @media only screen and (min-width: 992px) {
+    width: 50%;
+    position: relative; 
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  /* Extra large devices (large laptops and desktops, 1200px and up) */
+  @media only screen and (min-width: 1200px) {
+    width: 30%;
+    position: relative; 
+    left: 50%;
+    transform: translateX(-50%);
   }
 `;
 
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 const Widget = styled.div`
+  /* Extra small devices (phones, 600px and down) */
+  @media only screen and (max-width: 600px) {
     margin-top: 24px;
     margin-bottom: 24px;
     border: 1px solid ${({ theme }) => theme.colors.primary};
@@ -18,6 +20,40 @@ const Widget = styled.div`
         font-weight: 400;
         line-height: 1;
     }
+  }
+
+  /* Small devices (portrait tablets and large phones, 600px and up) */
+  @media only screen and (min-width: 600px) {
+    margin-top: 24px;
+    margin-bottom: 24px;
+    border: 1px solid ${({ theme }) => theme.colors.primary};
+    background-color: ${({ theme }) => theme.colors.mainBg};
+    border-radius: 4px;
+    overflow: hidden;
+    h1, h2, h3 {
+        font-size: 16px;
+        font-weight: 700;
+        line-height: 1;
+        margin-bottom: 0;
+    }
+    p {
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 1;
+    }
+  }
+
+  /* Medium devices (landscape tablets, 768px and up) */
+  @media only screen and (min-width: 768px) {
+  }
+
+  /* Large devices (laptops/desktops, 992px and up) */
+  @media only screen and (min-width: 992px) {
+  }
+
+  /* Extra large devices (large laptops and desktops, 1200px and up) */
+  @media only screen and (min-width: 1200px) {
+  }
 `;
 
 Widget.Header = styled.header`
